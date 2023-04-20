394081e7f26a1d7e127d588503468d10ba4d43ac
Yorran Carvalho <yorrancarvalho18@gmail.com>
2023-04-20 03:07:19 -0300

    first commit python

---
+++ password.py
@@ -0,0 +1,34 @@
+import random
+import PySimpleGUI as sg
+import os
+
+
+class PassGen:
+    def __init__(self):
+        # Layout
+        sg.theme('Black')
+        layout = [
+            [sg.Text('Site/Software', size=(10,1)),
+             sg.Input(key='site', size=(20,1))],
+            [sg.Text('E-mail/Usuário', size=(10,1)),
+             sg.Input(key='usuario', size=(20,1))],
+            [sg.Text('Quantidade de Caracteres'), sg.Combo(values=list(
+                range (30)), key='total_chars', default_value=1, size=(3,1))],
+            [sg.Output(size=(32,5))],
+            [sg.Button('Gerar Senha')]
+        ]
+        # Declarar Janela
+        self.janela = sg.Window('Password Generator', layout)
+
+    def Iniciar (self):
+        while True:
+            evento, valores = self.janela.read()
+            if evento == sg.WINDOW_CLOSED:
+                break
+
+    def salvar_senha(self):
+        pass
+
+
+gen = PassGen()
+gen.Iniciar()
