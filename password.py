7def8c61752e6affc4aca9c6a42fad9fc6ef7842
Yorran Carvalho <yorrancarvalho18@gmail.com>
2023-04-26 18:07:49 -0300

    fim do projeto

--- password.py
+++ password.py
@@ -25,9 +25,25 @@ class PassGen:
             evento, valores = self.janela.read()
             if evento == sg.WINDOW_CLOSED:
                 break
+            if evento == 'Gerar Senha':
+                nova_senha = self.gerar_senha(valores)
+                print(nova_senha)
+                self.salvar_senha(nova_senha, valores)
 
-    def salvar_senha(self):
-        pass
+
+
+    def gerar_senha(self, valores):
+        char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%¨&*'
+        chars = random.choices(char_list, k=int(valores['total_chars']))
+        new_pass = ''.join(chars)
+        return new_pass
+
+    def salvar_senha(self, nova_senha, valores):
+        with open('senhas.txt', 'a', newline='') as arquivo:
+            arquivo.write(
+                f"site: {valores['site']}, usuario: {valores['usuario']}, nova senha: {nova_senha}")
+
+        print('Arquivo Salvo')
 
 
 gen = PassGen()
